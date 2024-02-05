adc7be293146ee771aee8d6ae571d184f1ede4d4
The-CoderBoy <gauravjoshi747@gmail.com>
2024-02-05 12:10:51 +0530

    add new secontion

--- pages/index.js
+++ pages/index.js
@@ -5,30 +5,7 @@ import { montserrat } from "@/utils/font";
 import ServiceCard from "@/components/ServiceCard";
 import { FaArrowRightLong } from "react-icons/fa6";
 import Link from "next/link";
-
-const data = [
-  {
-    backgroundColor: "white",
-    textColor: "black",
-    imgSrc: "./images/website.webp",
-    heading: "Website Development",
-    text: "I help businesses come online via a good website. I build all kinds of websites, like e-commerce, blogging, beautiful landing pages.",
-  },
-  {
-    backgroundColor: "black",
-    textColor: "white",
-    imgSrc: "./images/web-application-img.png",
-    heading: "Web Application",
-    text: "I build robust and scalable applications that can deliver desirable functionality to thousands of people. i am expert in accounting, inventory management, and report presentation.",
-  },
-  {
-    backgroundColor: "white",
-    textColor: "black",
-    imgSrc: "./images/social media.webp",
-    heading: "Social Media Management",
-    text: "In today's world, social media is the best medium for marketing and brand building. i do the process of creating, scheduling, and analyzing content for my clients",
-  },
-];
+import data from "@/utils/data";
 
 function index() {
   return (
